b4d98837898adce88ac85f1ed5d0a4865cd9d32a
dougsuh <38517374+dougsuh@users.noreply.github.com>
2026-03-30 12:15:22 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
-let x,y;
+let x = 100, y = 200;
+let inp;
+let typedText = "";
+
 function setup() {
-  createCanvas(400, 400);
-  x = 200;
-  y = 200;
+  createCanvas(400, 300);
+
+  inp = createInput('');
+  inp.position(10, 320);
+  inp.attribute('placeholder', '문자를 입력하세요');
+  inp.input(() => {
+    typedText = inp.value();
+  });
+}
+function draw() {
+  background(240);
+  textSize(24);
+  text("입력값: " + typedText, 20, 50);
+  ellipse(x, y, 20, 20);
 }
 
-function draw() {
-  background(220);
-  ellipse(x,y,150,100);
-}
 function keyPressed() {
   if (key === 'w') y -= 5;
   else if (key === 'a') x -= 5;
