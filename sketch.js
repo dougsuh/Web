2b19ce4b3eb4d208fcb1f976182065a30ebafe2a
dougsuh <38517374+dougsuh@users.noreply.github.com>
2026-03-30 11:53:34 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
+let x,y;
 function setup() {
   createCanvas(400, 400);
+  x = 200;
+  y = 200;
 }
 
 function draw() {
   background(220);
-  ellipse(mouseX,mouseY,150,100);
+  ellipse(x,y,150,100);
+}
+function keyPressed() {
+  if (key === 'w') y -= 5;
+  else if (key === 'a') x -= 5;
+  else if (key === 's') y += 5;
+  else x += 5;
+}
+function mousePressed(){
+  x = mouseX;
+  y = mouseY;
 }
