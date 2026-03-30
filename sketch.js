a21bf6a8e0fe3671a76fbd61689e24d26d5b1467
dougsuh <38517374+dougsuh@users.noreply.github.com>
2026-03-30 13:18:31 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,8 @@
-let x = 100, y = 200;
-let inp;
-let typedText = "";
-
 function setup() {
-  createCanvas(400, 300);
-
-  inp = createInput('');
-  inp.position(10, 320);
-  inp.attribute('placeholder', '문자를 입력하세요');
-  inp.input(() => {
-    typedText = inp.value();
-  });
+  createCanvas(400, 400);
 }
+
 function draw() {
-  background(240);
-  textSize(24);
-  text("입력값: " + typedText, 20, 50);
-  ellipse(x, y, 20, 20);
-}
-
-function keyPressed() {
-  if (key === 'w') y -= 5;
-  else if (key === 'a') x -= 5;
-  else if (key === 's') y += 5;
-  else x += 5;
-}
-function mousePressed(){
-  x = mouseX;
-  y = mouseY;
+  background(220);
+  ellipse(200,200,300,200);
 }
